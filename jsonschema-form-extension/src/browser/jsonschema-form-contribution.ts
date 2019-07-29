cc1e9e5ecbe0ee1a10b39335ff1074b0567fced4
Alberto Iannaccone <a.iannaccone@arduino.cc>
2019-07-29 15:17:39 +0200

    add list files command contribution + menu + keybinding

--- jsonschema-form-extension/src/browser/jsonschema-form-contribution.ts
+++ jsonschema-form-extension/src/browser/jsonschema-form-contribution.ts
@@ -1,11 +1,16 @@
 import { injectable, inject } from "inversify";
 import { CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry, MessageService } from "@theia/core/lib/common";
-import { CommonMenus } from "@theia/core/lib/browser";
+import { CommonMenus, KeybindingContribution, KeybindingRegistry } from "@theia/core/lib/browser";
 
 export const JsonschemaFormCommand = {
     id: 'JsonschemaForm.command',
     label: "Shows a message"
 };
+
+export const ListFilesCommand = {
+    id: 'Arduino.ListFiles',
+    label: "List All Files"
+ };
 
 @injectable()
 export class JsonschemaFormCommandContribution implements CommandContribution {
@@ -28,6 +33,42 @@ export class JsonschemaFormMenuContribution implements MenuContribution {
         menus.registerMenuAction(CommonMenus.EDIT_FIND, {
             commandId: JsonschemaFormCommand.id,
             label: 'Say Hello'
+        });
+    }
+}
+
+@injectable()
+export class ListFilesCommandContribution implements CommandContribution {
+    constructor(
+        @inject(MessageService) private readonly messageService: MessageService,
+    ) { }
+
+    registerCommands(registry: CommandRegistry): void {
+        registry.registerCommand(ListFilesCommand, {
+            execute: () => this.messageService.info('Listing Files...')
+        });
+    }
+}
+
+@injectable()
+export class ListFilesMenuContribution implements MenuContribution {
+
+    registerMenus(menus: MenuModelRegistry): void {
+        menus.registerMenuAction(CommonMenus.EDIT_FIND, {
+            commandId: ListFilesCommand.id,
+            label: 'List Files'
+        });
+    }
+}
+
+
+@injectable()
+export class ListFilesKeybindingContribution implements KeybindingContribution {
+
+    registerKeybindings(keybinding: KeybindingRegistry): void {
+        keybinding.registerKeybinding({
+            command: ListFilesCommand.id,
+            keybinding: 'alt+shift+l'
         });
     }
 }
--- jsonschema-form-extension/src/browser/jsonschema-form-frontend-module.ts
+++ jsonschema-form-extension/src/browser/jsonschema-form-frontend-module.ts
@@ -1,4 +1,4 @@
-import { JsonschemaFormCommandContribution, JsonschemaFormMenuContribution } from './jsonschema-form-contribution';
+import { JsonschemaFormCommandContribution, JsonschemaFormMenuContribution, ListFilesCommandContribution, ListFilesMenuContribution, ListFilesKeybindingContribution } from './jsonschema-form-contribution';
 import {
     CommandContribution,
     MenuContribution
@@ -7,12 +7,17 @@ import { OpenHandler, WidgetFactory } from "@theia/core/lib/browser";
 import { ContainerModule } from "inversify";
 import { JsonschemaFormWidget, JsonschemaFormWidgetOptions } from './jsonschema-form-widget';
 import { JsonschemaFormOpenHandler } from './jsonschema-form-open-handler';
+import { KeybindingContribution } from "@theia/core/lib/browser";
 
 export default new ContainerModule(bind => {
     // add your contribution bindings here
 
     bind(CommandContribution).to(JsonschemaFormCommandContribution).inSingletonScope();
     bind(MenuContribution).to(JsonschemaFormMenuContribution).inSingletonScope();
+    bind(CommandContribution).to(ListFilesCommandContribution).inSingletonScope();
+    bind(MenuContribution).to(ListFilesMenuContribution).inSingletonScope();
+    bind(KeybindingContribution).to(ListFilesKeybindingContribution).inSingletonScope();
+    
 
     bind(OpenHandler).to(JsonschemaFormOpenHandler).inSingletonScope();
     bind(WidgetFactory).toDynamicValue(({ container }) => ({
